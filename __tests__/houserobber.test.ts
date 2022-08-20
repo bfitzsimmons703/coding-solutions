3fbd450f7a3054ccf35d51decb31c95409e24661
Brian Fitzsimmons <bfitzsimmons703@gmail.com>
2022-08-20 12:28:54 -0700

    Using DP template for house robber. Need to study up on DP

--- __tests__/houserobber.test.ts
+++ __tests__/houserobber.test.ts
@@ -7,13 +7,16 @@
 // Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
 
 export function rob(nums: number[]): number {
-	if (!nums.length) return 0;
+	const dp: number[] = [];
 
-	if (nums.length == 1) {
-		return nums[0];
+	dp[0] = 0;
+	dp[1] = nums[0];
+
+	for (let i = 1; i < nums.length; i++) {
+		dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
 	}
 
-	return Math.max(nums[0] + rob(nums.slice(2)), nums[1] + rob(nums.slice(3)));
+	return dp[nums.length];
 }
 
 test('House Robber', () => {
